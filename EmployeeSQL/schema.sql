635c99c6a1c3d423461a64eea26847e494a30733
emilyneaville <119391399+emilyneaville@users.noreply.github.com>
2023-03-02 21:26:37 -0700

    Updated schema

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,15 +1,15 @@
 CREATE TABLE "Dept_Emp" (
     "emp_no" INT   NOT NULL,
-    "dept_no" VARCHAR(8)   NOT NULL
+    "dept_no" VARCHAR   NOT NULL
 );
 
 CREATE TABLE "Employees" (
     "emp_no" INT   NOT NULL,
-    "title_id" VARCHAR(8)   NOT NULL,
+    "emp_title_id" VARCHAR   NOT NULL,
     "birth_date" DATE   NOT NULL,
-    "first_name" VARCHAR(35)   NOT NULL,
-    "last_name" VARCHAR(35)   NOT NULL,
-    "sex" VARCHAR(1)   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "sex" VARCHAR   NOT NULL,
     "hire_date" DATE   NOT NULL,
     CONSTRAINT "pk_Employees" PRIMARY KEY (
         "emp_no"
@@ -22,21 +22,21 @@ CREATE TABLE "Salaries" (
 );
 
 CREATE TABLE "Titles" (
-    "title_id" VARCHAR(8)   NOT NULL,
-    "title" VARCHAR(40)   NOT NULL,
+    "title_id" VARCHAR   NOT NULL,
+    "title" VARCHAR   NOT NULL,
     CONSTRAINT "pk_Titles" PRIMARY KEY (
         "title_id"
      )
 );
 
 CREATE TABLE "Dept_Manager" (
-    "dept_no" VARCHAR(8)   NOT NULL,
+    "dept_no" VARCHAR   NOT NULL,
     "emp_no" INT   NOT NULL
 );
 
 CREATE TABLE "Departments" (
-    "dept_no" VARCHAR(8)   NOT NULL,
-    "dept_name" VARCHAR(40)   NOT NULL,
+    "dept_no" VARCHAR   NOT NULL,
+    "dept_name" VARCHAR   NOT NULL,
     CONSTRAINT "pk_Departments" PRIMARY KEY (
         "dept_no"
      )
@@ -48,7 +48,7 @@ REFERENCES "Employees" ("emp_no");
 ALTER TABLE "Dept_Emp" ADD CONSTRAINT "fk_Dept_Emp_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "Departments" ("dept_no");
 
-ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_title_id" FOREIGN KEY("title_id")
+ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
 REFERENCES "Titles" ("title_id");
 
 ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
