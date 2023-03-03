64e974c04953d782f2041e7a393fb5b869eb0961
emilyneaville <119391399+emilyneaville@users.noreply.github.com>
2023-03-02 21:46:55 -0700

    Improved readbility

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -11,9 +11,7 @@ CREATE TABLE "Employees" (
     "last_name" VARCHAR   NOT NULL,
     "sex" VARCHAR   NOT NULL,
     "hire_date" DATE   NOT NULL,
-    CONSTRAINT "pk_Employees" PRIMARY KEY (
-        "emp_no"
-     )
+    CONSTRAINT "pk_Employees" PRIMARY KEY ("emp_no")
 );
 
 CREATE TABLE "Salaries" (
@@ -24,9 +22,7 @@ CREATE TABLE "Salaries" (
 CREATE TABLE "Titles" (
     "title_id" VARCHAR   NOT NULL,
     "title" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_Titles" PRIMARY KEY (
-        "title_id"
-     )
+    CONSTRAINT "pk_Titles" PRIMARY KEY ("title_id")
 );
 
 CREATE TABLE "Dept_Manager" (
@@ -37,9 +33,7 @@ CREATE TABLE "Dept_Manager" (
 CREATE TABLE "Departments" (
     "dept_no" VARCHAR   NOT NULL,
     "dept_name" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_Departments" PRIMARY KEY (
-        "dept_no"
-     )
+    CONSTRAINT "pk_Departments" PRIMARY KEY ("dept_no")
 );
 
 ALTER TABLE "Dept_Emp" ADD CONSTRAINT "fk_Dept_Emp_emp_no" FOREIGN KEY("emp_no")
